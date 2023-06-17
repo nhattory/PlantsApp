036f1858d41ad7264e10c0f5b1d003f2548bf349
luannv <luannv@skcompany.vn.com>
2023-06-17 21:51:41 +0700

    update add

--- app/src/main/java/com/example/plantsapp/HomeActivity.java
+++ app/src/main/java/com/example/plantsapp/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.plantsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -10,9 +11,12 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
+    FloatingActionButton btn_add;
+    private Fragment selectedFragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,25 +29,24 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragmentContainerView, new fm_home(this))
                 .commit();
+        btn_add = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
+        btn_add.setOnClickListener(HomeActivity.this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment selectedFragment = null;
+
         switch (item.getItemId()) {
-            case R.id.btn_home:
+            case R.id.home:
                 System.out.println("profile");
                 selectedFragment = new fm_home(this);
                 break;
-            case R.id.btn_profile:
+            case R.id.person:
                 System.out.println("profile");
                 selectedFragment = new fm_profile();
                 break;
-            case R.id.btn_add:
-                System.out.println("add");
-                selectedFragment = new fm_add(this);
-                break;
         }
+
         if (selectedFragment != null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragmentContainerView, selectedFragment)
@@ -52,4 +55,16 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         }
         return false;
     }
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.floatingActionButton2){
+            selectedFragment = new fm_add(this);
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, selectedFragment)
+                        .commit();
+            }
+        }
+    }
+
 }
